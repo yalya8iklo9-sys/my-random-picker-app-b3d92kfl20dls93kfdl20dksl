855a132680cd72b5ed0a28b54e48497115121812
yalya8iklo9-sys <yalya8ik.lo9@gmail.com>
2025-12-12 01:07:26 +0900

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "manifest.webmanifest"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
