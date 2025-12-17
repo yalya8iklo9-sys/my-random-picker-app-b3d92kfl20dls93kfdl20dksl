30a615560d5b24508a876e72ec9de4f628c73163
yalya8iklo9-sys <yalya8ik.lo9@gmail.com>
2025-12-17 18:47:30 +0900

    sw.js の更新

---
+++ test/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
